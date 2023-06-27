63a64280c436eb300dbfb00bc71d27d9d4ea671c
sockysec <121141737+sockysec@users.noreply.github.com>
2023-06-27 14:07:53 +1200

    Update automatr.py

--- automatr.py
+++ automatr.py
@@ -20,15 +20,19 @@ print("This script automates the manual process of opening URLs.")
 while True:
     print("\nOptions:")
     print("1. Open a list of social media search URLs within your browser.")
-    print("2. Configure a list of social media search URLs based on an input list of names.")
+    print("2. Search for a name across social media.")
+    print("3. Configure a list of social media search URLs based on an input list of names.")
     option = input("Select an option (1 or 2): ")
 
     if option == "1":
         subprocess.call(["python3", "runscan.py"])
         break
     elif option == "2":
+        subprocess.call(["python3", "target.py"])
+        break
+    elif option == "3":
         subprocess.call(["python3", "createlist.py"])
         break
     else:
-        print("Invalid option. Please choose 1 or 2.")
+        print("Invalid option. Please choose 1, 2, or 3.")
 
